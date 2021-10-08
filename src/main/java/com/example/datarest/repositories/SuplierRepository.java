65f1aba8146e3b062fef55a75d454f047f23bb7f
muhammadqodir1007 <89183031+muhammadqodir1007@users.noreply.github.com>
2021-10-08 15:58:19 +0300

    Add files via upload

--- src/main/java/com/example/datarest/repositories/SuplierRepository.java
+++ src/main/java/com/example/datarest/repositories/SuplierRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "category",excerptProjection = CategoryProjection.class)
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
+public interface SuplierRepository extends JpaRepository<Category,Integer> {
 }
